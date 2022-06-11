56635d5a5f1146ac8d1309c4e8d79c3fec0b3267
Vincent Herlemont <vincent@herlemont.fr>
2022-06-11 12:24:46 +0200

    Init.

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+NAME=$1
+if [[ -z $NAME ]]; then
+  echo "Please provide a name."
+fi
+
+URL=$2
+if [[ -z $URL ]]; then
+  echo "Please provide an url of the archive .tar.gz."
+fi
+
+
+file="$NAME.tar.gz"
+curl -L "$URL" --output "$file"
+mkdir "$NAME"
+tar -xzvf "$file" --strip-components=1 -C "$NAME"
+rm "$file"
